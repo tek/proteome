ca8da31394df173f30cd398ed3463abffb2bb02d
Torsten Schmits <torstenschmits@gmail.com>
2018-12-08 02:00:18 +0100

    `forkNeovim`

--- lib/Ribosome/Internal/IO.hs
+++ lib/Ribosome/Internal/IO.hs
@@ -1,11 +1,14 @@
 module Ribosome.Internal.IO(
   retypeNeovim,
+  forkNeovim,
 ) where
 
+import GHC.Conc.Sync (forkIO)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ask, runReaderT, withReaderT)
 import Control.Monad.Trans.Resource (runResourceT)
-import Neovim.Context.Internal (Neovim(..), Config(..), retypeConfig)
+import Data.Functor (void)
+import Neovim.Context.Internal (Neovim(..), Config(..), retypeConfig, runNeovim)
 
 -- try using Contravariant with this
 retypeNeovim :: (e0 -> e1) -> Neovim e1 a -> Neovim e0 a
@@ -14,3 +17,9 @@ retypeNeovim transform thunk = do
   liftIO $ runReaderT (withReaderT (newEnv env) $ runResourceT $ unNeovim thunk) env
   where
     newEnv = retypeConfig . transform . customConfig
+
+forkNeovim :: Neovim e () -> Neovim e ()
+forkNeovim thunk = do
+  env <- Neovim ask
+  _ <- liftIO $ forkIO $ void $ runNeovim env thunk
+  return ()
