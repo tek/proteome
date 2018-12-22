083516608577532fc9f450a11d4fb78dc5e43fb2
Torsten Schmits <torstenschmits@gmail.com>
2018-12-22 13:34:34 +0100

    helpers for option/call_function with explicit type and list splitting

--- lib/Ribosome/Api/Option.hs
+++ lib/Ribosome/Api/Option.hs
@@ -1,14 +1,30 @@
 module Ribosome.Api.Option(
   optionCat,
-  rtpCat
+  rtpCat,
+  optionList,
+  option,
+  optionString,
 ) where
 
-import Neovim
+import Control.Monad ((>=>))
+import Data.List.Split (splitOn)
+import Neovim (Neovim, vim_get_option', fromObject', vim_set_option', toObject, NvimObject)
 
-optionCat :: String -> String -> Neovim env ()
+optionCat :: String -> String -> Neovim e ()
 optionCat name extra = do
   current <- vim_get_option' name >>= fromObject'
   vim_set_option' name $ toObject $ current ++ "," ++ extra
 
-rtpCat :: String -> Neovim env ()
+rtpCat :: String -> Neovim e ()
 rtpCat = optionCat "runtimepath"
+
+option :: NvimObject a => String -> Neovim e a
+option = vim_get_option' >=> fromObject'
+
+optionString :: String -> Neovim e String
+optionString = option
+
+optionList :: String -> Neovim e [String]
+optionList name = do
+  s <- option name
+  return $ splitOn "," s
