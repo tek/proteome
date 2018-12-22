f132774aa2ffbdf93bc659ba08e061574ba1fc3b
Torsten Schmits <torstenschmits@gmail.com>
2018-12-22 17:04:40 +0100

    show extra projects and error log in `ProDiag`

--- lib/Proteome/Diag.hs
+++ lib/Proteome/Diag.hs
@@ -4,12 +4,14 @@ module Proteome.Diag(
 
 import Data.Functor (void)
 import Data.List (intercalate)
+import Data.Map (foldMapWithKey)
 import Neovim (CommandArguments)
 import Ribosome.Data.ScratchOptions (defaultScratchOptions)
+import Ribosome.Data.Errors (Error(Error), Errors(Errors), ComponentName(ComponentName))
 import Ribosome.Scratch (showInScratch)
 import qualified Ribosome.Data.Ribo as Ribo (inspect)
 import Proteome.Env (getMainProject)
-import Proteome.Data.Env(configLog)
+import qualified Proteome.Data.Env as Env (configLog, projects, errors)
 import Proteome.Data.Proteome (Proteome)
 import Proteome.Data.Project (
   Project (Project),
@@ -49,11 +51,44 @@ formatMain (Project meta types lang langs) = do
     "languages: " ++ intercalate ", " (fmap projectLang langs)
     ]
 
+formatExtraProjects :: [Project] -> Proteome [String]
+formatExtraProjects projects = do
+  formatted <- traverse formatMain projects
+  return $ ["", "Extra projects", ""] ++ intercalate [""] formatted
+
+formatExtraProjectsIfNonempty :: Proteome [String]
+formatExtraProjectsIfNonempty = do
+  projects <- Ribo.inspect Env.projects
+  case projects of
+    _ : _ -> formatExtraProjects projects
+    _ -> return []
+
+formatError :: Error -> [String]
+formatError (Error stamp (first:message)) = (show stamp ++ " | " ++ first) : message
+formatError _ = []
+
+formatComponentErrors :: ComponentName -> [Error] -> [String]
+formatComponentErrors (ComponentName name) errors@(_ : _) =
+  [name ++ "", ""] ++ (errors >>= formatError)
+formatComponentErrors _ _ = []
+
+
+formatErrorLog :: Errors -> [String]
+formatErrorLog (Errors errors) =
+  case compErrors of
+    _ : _ -> ["", "Errors", ""] ++ compErrors
+    _ -> []
+  where
+    compErrors = foldMapWithKey formatComponentErrors errors
+
 diagnostics :: Proteome [String]
 diagnostics = do
   main <- getMainProject >>= formatMain
-  confLog <- Ribo.inspect configLog
-  return $ ["Diagnostics", "", "Main project:"] ++ main ++ ["", "loaded config files:"] ++ confLog
+  extra <- formatExtraProjectsIfNonempty
+  confLog <- Ribo.inspect Env.configLog
+  errors <- Ribo.inspect Env.errors
+  return $ ["Diagnostics", "", "Main project", ""] ++ main ++ extra ++ ["", "loaded config files:"] ++
+    confLog ++ formatErrorLog errors
 
 proDiag :: CommandArguments -> Proteome ()
 proDiag _ = do
