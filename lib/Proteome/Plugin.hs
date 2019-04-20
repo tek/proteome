54fd4aebeb4fec200e21c78410493abab428ffb9
Torsten Schmits <torstenschmits@gmail.com>
2019-04-20 14:29:06 +0200

    make init stages sync

--- lib/Proteome/Plugin.hs
+++ lib/Proteome/Plugin.hs
@@ -7,32 +7,33 @@ module Proteome.Plugin(
 where
 
 import Data.Default.Class (Default(def))
-import UnliftIO.STM (TVar)
 import Neovim (
-  Plugin(..),
-  function,
-  function',
-  command,
-  command',
-  autocmd,
+  CommandOption(CmdBang),
   Neovim,
-  StartupConfig,
   NeovimConfig,
   NeovimPlugin,
+  Plugin(..),
+  StartupConfig,
   Synchronous(..),
+  autocmd,
+  command,
+  command',
+  function,
+  function',
   wrapPlugin,
-  CommandOption(CmdBang),
   )
 import Ribosome.Control.Ribosome (Ribosome)
-import Proteome.Init (proteomePoll, proteomeStage1, proteomeStage2, proteomeStage4, initialize)
-import Proteome.Data.Env (Env)
+import UnliftIO.STM (TVar)
+
 import Proteome.Add (proAdd, proAddCmd)
-import Proteome.Config (proReadConfig)
-import Proteome.Tags (proTags)
-import Proteome.Save (proSave)
 import Proteome.BufEnter (bufEnter)
-import Proteome.Project.Activate (proNext, proPrev)
+import Proteome.Config (proReadConfig)
+import Proteome.Data.Env (Env)
 import Proteome.Diag (proDiag)
+import Proteome.Init (initialize, proteomePoll, proteomeStage1, proteomeStage2, proteomeStage4)
+import Proteome.Project.Activate (proNext, proPrev)
+import Proteome.Save (proSave)
+import Proteome.Tags (proTags)
 
 plugin' :: Ribosome (TVar Env) -> Plugin (Ribosome (TVar Env))
 plugin' env =
@@ -40,9 +41,9 @@ plugin' env =
     environment = env,
     exports = [
       $(function' 'proteomePoll) Sync,
-      $(function' 'proteomeStage1) Async,
-      $(function' 'proteomeStage2) Async,
-      $(function' 'proteomeStage4) Async,
+      $(function' 'proteomeStage1) Sync,
+      $(function' 'proteomeStage2) Sync,
+      $(function' 'proteomeStage4) Sync,
       $(function "ProAddProject" 'proAdd) Sync,
       $(command "ProAdd" 'proAddCmd) [CmdBang],
       $(function' 'proSave) Async,
