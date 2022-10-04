a21b69a3efb986e599c9a68e454b67323b476b95
Torsten Schmits <git@tryp.io>
2022-10-04 17:51:28 +0200

    use `String` as default highlight for tag names

--- packages/proteome/lib/Proteome/Tags/Syntax.hs
+++ packages/proteome/lib/Proteome/Tags/Syntax.hs
@@ -6,5 +6,7 @@ import Ribosome.Syntax (HiLink (HiLink), Syntax, build, link, match, prefix, (#>
 tagsSyntax :: Syntax
 tagsSyntax =
   (#links <>~ [HiLink "Module" "Type"]) $ build $ prefix "ProTags" $
-  match "Line" "^.*$" #> match "Name" [exon|🟣 \S\+|] >- link "Directory" (match "Package" [exon|📦 \S\+|]) >-
+  match "Line" "^.*$" #>
+    link "String" (match "Name" [exon|🟣 \S\+|]) >-
+    link "Directory" (match "Package" [exon|📦 \S\+|]) >-
     link "Module" (match "Module" [exon|\S\+|])
